dc75f47940614e4496f62e29f3f4cc9ed9b45f58
daylonblakely <daylonblakely@gmail.com>
2024-03-21 16:17:17 -0700

    small circle skew

--- src/components/AnimatedBgCircle.jsx
+++ src/components/AnimatedBgCircle.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { styled } from '@mui/system';
 import { motion, useMotionValue, useTransform } from 'framer-motion';
 
@@ -10,24 +10,29 @@ const AnimatedCircle = styled(motion.div)(({ theme }) => ({
   position: 'absolute',
   zIndex: -1,
   boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.25)',
-
   [theme.breakpoints.up('md')]: {
     marginTop: '15px',
   },
 }));
 
 const AnimatedBgCircle = () => {
-  const mouseX = useMotionValue(window.innerWidth / 2);
-  const mouseY = useMotionValue(window.innerHeight / 2);
-
-  // Use the mouse position to scale the blob
-  const scale = useTransform(mouseX, [0, window.innerWidth], [1, 1.5]);
-  const skewX = useTransform(mouseY, [0, window.innerHeight], [0, 30]);
+  const ref = useRef(null);
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
 
   useEffect(() => {
     const handleMouseMove = (event) => {
-      mouseX.set(event.clientX);
-      mouseY.set(event.clientY);
+      if (ref.current) {
+        const rect = ref.current.getBoundingClientRect();
+        const centerX = rect.left + rect.width / 2;
+        const centerY = rect.top + rect.height / 2;
+        const deltaX = event.clientX - centerX;
+        const deltaY = event.clientY - centerY;
+
+        // Calculate skew based on distance from center of the circle
+        mouseX.set((deltaX * 5) / centerX);
+        mouseY.set((deltaY * 5) / centerY);
+      }
     };
 
     window.addEventListener('mousemove', handleMouseMove);
@@ -35,13 +40,14 @@ const AnimatedBgCircle = () => {
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
     };
-  }, [mouseX, mouseY]);
+  }, []);
 
   return (
     <AnimatedCircle
+      ref={ref}
       style={{
-        scaleX: scale,
-        skewX: skewX,
+        skewX: mouseX,
+        skewY: mouseY,
       }}
     />
   );
